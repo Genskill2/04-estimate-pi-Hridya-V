a17030181878e9f7de4297fe8b538c67884f00c4
Hridya V <71560391+Hridya-V@users.noreply.github.com>
2021-05-30 13:41:30 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -24,5 +24,16 @@ int main(void) {
   }
 }
 
- 
+ float wallis_pi(int n)
+{
+float piby2=1.0;
+for (int j=1;j<=n;j++)
+{
+int m=4*j*j;
+float t=float(m)/(float(m)-1);
+piby2*=t;
+}
+return (2*piby2);
+}
+
 
